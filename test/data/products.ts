1e9108309b0011bd0afd20641f74e80e39d8cdd0
Oscar Benito <obenito@datiobd.com>
2023-06-27 20:33:23 +0200

    0.0.1 ready

---
+++ test/data/products.ts
@@ -0,0 +1,10 @@
+export const product1 = {
+    id: 'ABC',
+    title: 'Coffee Mug - Sin imagen',
+}
+
+export const product2 = {
+    id: 'DEF',
+    title: 'Coffee Mug - Con Imagen ',
+    img: './coffee-mug.png'
+}
